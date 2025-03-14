47525237269d06b8a1de81ea0b1a43f43d5f0a6c
Steven Luiz <steveluizcontato@gmail.com>
2025-03-14 19:28:29 -0300

    relogio v2 14/03/2025

---
+++ Relogio/js/relogio.js
@@ -0,0 +1,17 @@
+function hour(){
+    setInterval(function(){
+    let date = new Date()
+    let hour = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+
+    if(date.getSeconds() < 10){
+        sec = "0"+date.getSeconds()
+    } if(date.getMinutes() < 10){
+        min = "0"+date.getMinutes()
+    } if(date.getHours() < 10){
+        hour = "0"+date.getHours()
+    } let thour = document.getElementById("icron")
+    thour.innerHTML = `${hour}:${min}:${sec}`
+}, 1000)
+}
