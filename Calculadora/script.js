f2bcd1552f06468a03e42f40d5254461eee2a2dd
Steven Luiz <steveluizcontato@gmail.com>
2025-04-10 18:44:40 -0300

    calculadora v0.8

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,9 +1,23 @@
-var result = document.getElementById('calc')
+let result = document.getElementById('calc')
 
 function insert(num) {
     result.innerHTML += num
 }
 
-function bakc(){
-    
+function back(){
+    let volta = document.getElementById('calc').innerHTML
+    document.getElementById('calc').innerHTML = volta.substring(0, volta.length -1)
+}
+
+function clean(){
+    result.innerHTML = ""
+}
+
+function calc(){
+    let total = document.getElementById('calc').innerHTML
+    if(total){
+    document.getElementById('calc').innerHTML = eval(total)
+    } else{
+        alert('Erro')
+    }
 }
