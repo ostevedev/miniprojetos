4f07a02afa1a2259d90b99031b10ecb0e9e1a244
Steven Luiz <steveluizcontato@gmail.com>
2025-09-03 17:11:29 -0300

    clone-airbnb v0.1
    navbar criado

---
+++ clone-airbnb/src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap'
+import './App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// Componentes
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ clone-airbnb/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './css/Navbar.css'
+import logo from '../assets/airbnb.svg'
+
+export default function Navbar() {
+    return (
+        <div >
+            <nav className='nav-topo'>
+                <div className='m-auto container-airbnb'>
+                    <div className='d-flex col-sm-6'>
+                        <img className='logo' src={logo} alt="Logo airbnb" />
+                    </div>
+                    <div className='d-flex justify-content-end col-sm-6'>
+                        <a className='navlink' href="">Seja um anfitrião</a>
+                        <a className='globo navlink mx-2' href=""><i className="bi bi-globe"></i></a>
+                        <div className="dropdown d-flex align-itens-center">
+                            <a className="btn-login" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-list fs-5 ps-2 me-1"></i>
+                                <i class="bi bi-person-circle position-relative fs-2 pe-2"><span class="position-absolute top-0 start-50 badge border border-light rounded-circle bg-danger p-2"><span class="visually-hidden">unread messages</span></span></i>
+                            </a>
+
+                            <ul className="dropdown-menu mt-2 py-3 border-0 shadow">
+                                <li><a className="dropdown-item px-3 p-2 fw-bold" href="#">Cadastre-se</a></li>
+                                <li><a className="dropdown-item px-3 p-2 " href="#">Entrar</a></li>
+                                <li><hr className='dropdown-divider' /></li>
+                                <li><a className="dropdown-item px-3 p-2 " href="#">Hospede em sua acomodação</a></li>
+                                <li><a className="dropdown-item px-3 p-2 " href="#">Hospede uma experiência</a></li>
+                                <li><a className="dropdown-item px-3 p-2 " href="#">Ajuda</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
