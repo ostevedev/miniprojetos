ea82d83288891ff749df929530dbe3bb69f195d6
Steven Luiz <steveluizcontato@gmail.com>
2025-09-16 10:40:07 -0300

    clone-airbnb att
    componente categorias finalizado

---
+++ clone-airbnb/backend/dados.js
@@ -0,0 +1,249 @@
+export const categorias = [
+    {
+        id: 1,
+        imagem: 'https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg',
+        titulo: 'Em frente à praia'
+    },
+    {
+        id: 2,
+        imagem: 'https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg',
+        titulo: 'Chalés'
+    },
+    {
+        id: 3,
+        imagem: 'https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg',
+        titulo: 'Vistas incríveis'
+    },
+    {
+        id: 4,
+        imagem: 'https://a0.muscache.com/pictures/677a041d-7264-4c45-bb72-52bff21eb6e8.jpg',
+        titulo: 'Na beira do lago'
+    },
+    {
+        id: 5,
+        imagem: 'https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg',
+        titulo: 'Quartos privativos'
+    },
+    {
+        id: 6,
+        imagem: 'https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg',
+        titulo: 'Piscinas incríveis'
+    },
+    {
+        id: 7,
+        imagem: 'https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg',
+        titulo: 'No interior'
+    },
+    {
+        id: 8,
+        imagem: 'https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg',
+        titulo: 'Fazendas'
+    },
+    {
+        id: 9,
+        imagem: 'https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg',
+        titulo: 'Pousadas'
+    },
+    {
+        id: 10,
+        imagem: 'https://a0.muscache.com/pictures/35919456-df89-4024-ad50-5fcb7a472df9.jpg',
+        titulo: 'Microcasas'
+    },
+    {
+        id: 11,
+        imagem: 'https://a0.muscache.com/pictures/a4634ca6-1407-4864-ab97-6e141967d782.jpg',
+        titulo: 'Lago'
+    },
+    {
+        id: 12,
+        imagem: 'https://a0.muscache.com/pictures/1d477273-96d6-4819-9bda-9085f809dad3.jpg',
+        titulo: 'Casas triangulares'
+    },
+    {
+        id: 13,
+        imagem: 'https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg',
+        titulo: 'Uau!'
+    },
+    {
+        id: 14,
+        imagem: 'https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg',
+        titulo: 'Mansões'
+    },
+    {
+        id: 15,
+        imagem: 'https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg',
+        titulo: 'Ilhas'
+    },
+    {
+        id: 16,
+        imagem: 'https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg',
+        titulo: 'Castelos'
+    },
+    {
+        id: 17,
+        imagem: 'https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg',
+        titulo: 'Luxe'
+    },
+    {
+        id: 18,
+        imagem: 'https://a0.muscache.com/pictures/4d4a4eba-c7e4-43eb-9ce2-95e1d200d10e.jpg',
+        titulo: 'Casas na árvore'
+    },
+    {
+        id: 19,
+        imagem: 'https://a0.muscache.com/pictures/c0a24c04-ce1f-490c-833f-987613930eca.jpg',
+        titulo: 'Parques nacionais'
+    },
+    {
+        id: 20,
+        imagem: 'https://a0.muscache.com/pictures/60ff02ae-d4a2-4d18-a120-0dd274a95925.jpg',
+        titulo: 'Vinhedos'
+    },
+    {
+        id: 21,
+        imagem: 'https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg',
+        titulo: 'Tropical'
+    },
+    {
+        id: 22,
+        imagem: 'https://a0.muscache.com/pictures/0ff9740e-52a2-4cd5-ae5a-94e1bfb560d6.jpg',
+        titulo: 'Contêineres'
+    },
+    {
+        id: 23,
+        imagem: 'https://a0.muscache.com/pictures/ca25c7f3-0d1f-432b-9efa-b9f5dc6d8770.jpg',
+        titulo: 'Acampamentos'
+    },
+    {
+        id: 24,
+        imagem: 'https://a0.muscache.com/pictures/957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg',
+        titulo: 'Surf'
+    },
+    {
+        id: 25,
+        imagem: 'https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg',
+        titulo: 'Cidades famosas'
+    },
+    {
+        id: 26,
+        imagem: 'https://a0.muscache.com/pictures/f60700bc-8ab5-424c-912b-6ef17abc479a.jpg',
+        titulo: 'Celeiros'
+    },
+    {
+        id: 27,
+        imagem: 'https://a0.muscache.com/pictures/4221e293-4770-4ea8-a4fa-9972158d4004.jpg',
+        titulo: 'Grutas'
+    },
+    {
+        id: 28,
+        imagem: 'https://a0.muscache.com/pictures/687a8682-68b3-4f21-8d71-3c3aef6c1110.jpg',
+        titulo: 'Barcos'
+    },
+]
+
+export const acomodacoes = [
+    {
+        id: 1,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/baf536ff-b76d-4b62-91ed-9fdc4107bf52.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/3b8adc7d-e152-4e44-be41-3fb50cc994cd.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/81abc3be-ef4a-480a-a2d8-382d590addc0.jpg?im_w=720'
+        ],
+        cidade: 'Florianópolis',
+        pais: 'Brasil',
+        preco: 1286,
+        nota: '4,7'
+    },
+    {
+        id: 2,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/68d7cace-6e07-417b-9817-1885b93f90ee.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/903a1c9b-2248-46a1-94c9-7207c44fc95b.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/a7fb61e9-0c04-43a1-83cd-efaeb9a2ae17.jpg?im_w=720'
+        ],
+        cidade: 'Bombas',
+        pais: 'Brasil',
+        preco: 399,
+        nota: '4,91'
+    },
+    {
+        id: 3,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/cb6c9158-4219-45ce-bcb9-4cdcad4275f5.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/4aea292e-20dc-4462-9408-9ac016077f01.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/9a89dca2-6c7f-40e8-9f7c-0391c96983ce.jpg?im_w=720'
+        ],
+        cidade: 'Governador Celso Ramos',
+        pais: 'Brasil',
+        preco: 460,
+        nota: '5,0'
+    },
+    {
+        id: 4,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/7ddf420c-61a0-4215-a3df-3e107ba31728.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/4b010075-6af9-40a5-81e3-03fc5d31678b.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/c4fe3f18-ca7b-4eb4-b8c5-b8f4a4f20512.jpg?im_w=720'
+        ],
+        cidade: 'Pioneiros',
+        pais: 'Brasil',
+        preco: 366,
+        nota: '5,0'
+    },
+    {
+        id: 5,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/miso/Hosting-52666674/original/12708288-a629-4a2b-9497-631b22fa19b8.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-52666674/original/0ac72dd9-aed2-47f0-8d41-874020f88de2.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-52666674/original/83bc2774-8b9d-4240-b76c-4d1849bdc480.jpeg?im_w=720'
+        ],
+        cidade: 'Porto Belo',
+        pais: 'Brasil',
+        preco: 589,
+        nota: '4,2'
+    },
+    {
+        id: 6,
+        categoria: 1,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/ec2bbbea-7982-4f23-9515-a17281ab75c3.jpg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-14834202/original/1c71bde1-660f-45c8-8daa-2f36334d4c21.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/8bbc6e59-d643-47bd-92c6-dc120cf686bd.jpg?im_w=720'
+        ],
+        cidade: 'Bombinhas',
+        pais: 'Brasil',
+        preco: 1299,
+        nota: '4,9'
+    },
+    {
+        id: 7,
+        categoria: 2,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/miso/Hosting-43333686/original/b6f40ac4-3468-443e-81d8-ce3b6f526943.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-43333686/original/ce61507f-5a20-46e1-93b9-098faacaa6de.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-43333686/original/81cb677f-65da-420e-a5f6-acd4fa188815.jpeg?im_w=720'
+        ],
+        cidade: 'São Bento do Sul',
+        pais: 'Brasil',
+        preco: 723,
+        nota: '4,97'
+    },
+    {
+        id: 8,
+        categoria: 2,
+        imagens: [
+            'https://a0.muscache.com/im/pictures/miso/Hosting-22319550/original/cc1fd71d-1a91-4026-99f8-c2cbffefd68f.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-22319550/original/e08d07a2-4eaa-4930-9c22-132072aee408.jpeg?im_w=720',
+            'https://a0.muscache.com/im/pictures/miso/Hosting-22319550/original/839c4a53-0cd8-447e-adb7-fe5bdcff7bf6.jpeg?im_w=720'
+        ],
+        cidade: 'Paraty',
+        pais: 'Brasil',
+        preco: 1160,
+        nota: '4,93'
+    },
+]
--- clone-airbnb/src/components/Categorias.jsx
+++ clone-airbnb/src/components/Categorias.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
+import { useState } from 'react';
+
 // Swiper 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 
 // Swiper css
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-import './css/Categorias.css'
+import './css/Categorias.css';
+
+import {categorias} from '../../backend/dados';
 
 export default function Categorias() {
+
+  const [idClidado, setIdClidado] = useState(1);
+
+  const handleClick = (e,id) =>{
+    setIdClidado(id)
+  }
+
   return (
-    <div className='container-fluid'>
-      <div className='container-airbnb row'>
+    <div className='pt-2 container-fluid d-flex justify-content-between align-items-center'>
+      <div className='container-airbnb d-flex align-items-center row'>
         <div className='col-sm-11'>
-          <Swiper slidesPerView={3} spaceBetween={30} pagination={{clickable: true,}} modules={[Pagination]}className="mySwiper">
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
+          <Swiper slidesPerView={14} slidesPerGroup={13} spaceBetween={7} navigation={true} modules={[Pagination, Navigation]}className="mySwiper">
+            {
+              categorias.map((dados, i)=>(
+                <SwiperSlide key={dados.id} virtualIndex={i} className={dados.id === idClidado ? 'active' : ''} onClick={(e)=> handleClick(e, dados.id)}>
+                  <img src={dados.imagem}/>
+                  <span>{dados.titulo}</span> 
+                </SwiperSlide>
+              ))
+            }
           </Swiper>
         </div>
-        <div className='col-sm-1'></div>
+        <div className='col-sm-1'>
+          Filtros
+        </div>
       </div>
     </div>
   ) 
