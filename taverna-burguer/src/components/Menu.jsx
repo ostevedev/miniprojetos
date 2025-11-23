b94d2fc8df0a1567b50a15665232024d7d97c8f0
Steven Luiz <steveluizcontato@gmail.com>
2025-11-23 16:12:24 -0300

    menu
    inicio do desenvolvimento do cardapio

--- taverna-burguer/src/components/Menu.jsx
+++ taverna-burguer/src/components/Menu.jsx
@@ -4,8 +4,15 @@ export default function Menu() {
   return (
     <div>
       <div className='inicio mt-'></div>
-      <div className='container cardapio'>
-        
+      <div className='container cardapio mt-4'>
+        <div class="card">
+          <img src="/img/bcheese.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
       </div>
     </div>
   )
