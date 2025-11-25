91aa2bf405657d8c44a2a1cbb7c8d3badc906c62
Steven Luiz <steveluizcontato@gmail.com>
2025-11-25 18:43:02 -0300

    Update Menu.jsx

--- taverna-burguer/src/components/Menu.jsx
+++ taverna-burguer/src/components/Menu.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>
-      <div className='inicio mt-'></div>
-      <div className='container cardapio mt-4'>
+    <div className=''>
+      <div className='inicio '></div>
+      <div className='container row cardapio mt-4'>
+        <div class="card">
+          <img src="/img/bcheese.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to make up the bulk of the card’s content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
         <div class="card">
           <img src="/img/bcheese.jpg" class="card-img-top" alt="..." />
           <div class="card-body">
